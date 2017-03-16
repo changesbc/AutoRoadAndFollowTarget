eb0bf2df9a7286bb4683feb7e4e88b1212480348
zhaobaocheng <changeszbc@gmail.com>
2017-03-16 20:22:44 +0800

    控制相机的跟随

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float velocity = 5;
+    private Rigidbody rigidbody;
+	void Start () {
+        rigidbody = this.GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 vel = rigidbody.velocity;
+
+
+       rigidbody.velocity = new Vector3(h * velocity, vel.y, v * velocity);
+
+    }
+}
